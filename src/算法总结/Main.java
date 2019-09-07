7ac4c68b2dbb5cc12d6295d724aac764a98cd9c5
zhanghanlun <zhanghanlun1@gmail.com>
2019-09-07 20:54:39 +0800

    题目二

--- src/算法总结/Main.java
+++ src/算法总结/Main.java
@@ -10,23 +10,29 @@ public class Main {
         int m = scanner.nextInt();
         int base = n + m - 1;
         int up = n - 1;
-        int result = (int) (getCount1(m + 1, base) / getCount(up)) % 1000000007;
+        int result = getResult(base,up);
         System.out.println(result);
     }
 
-    public static double getCount(int n) {
-        double count = 1;
-        for (int i = 1; i <= n; i++) {
-            count = count * i;
+    private static int getResult(int base, int up) {
+        double result = 1.0;
+        int count = base - up + 1;
+        for (int i = up; i >= 1; i--) {
+            if (base <= count){
+                result = result/up;
+            }else{
+                result = result * base / up;
+                base--;
+            }
+            if (result>1000000007){
+                result = result%1000000007;
+            }
         }
-        return count;
-    }
-
-    public static double getCount1(int start, int end) {
-        double count = start;
-        for (int i = start + 1; i <= end; i++) {
-            count = count * i;
+        if (base>=count){
+            for (int i = count;i<=base;i++){
+                result = result*i%1000000007;
+            }
         }
-        return count;
+        return (int)result;
     }
 }
