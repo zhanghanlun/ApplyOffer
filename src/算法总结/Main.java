ee8aae59bf0299e1c88196ee71a1345a8a9965f5
zhanghanlun <zhanghanlun1@gmail.com>
2019-09-08 14:52:51 +0800

    xiugai

--- src/算法总结/Main.java
+++ src/算法总结/Main.java
@@ -5,34 +5,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        int base = n + m - 1;
-        int up = n - 1;
-        int result = getResult(base,up);
-        System.out.println(result);
-    }
-
-    private static int getResult(int base, int up) {
-        double result = 1.0;
-        int count = base - up + 1;
-        for (int i = up; i >= 1; i--) {
-            if (base <= count){
-                result = result/up;
-            }else{
-                result = result * base / up;
-                base--;
-            }
-            if (result>1000000007){
-                result = result%1000000007;
-            }
-        }
-        if (base>=count){
-            for (int i = count;i<=base;i++){
-                result = result*i%1000000007;
-            }
-        }
-        return (int)result;
+//        Scanner scanner = new Scanner(System.in);
+//        int n = scanner.nextInt();
+//        int m = scanner.nextInt();
+//        int base = n + m - 1;
+//        int up = n - 1;
+//        int[][] C = new int[base + 11][base + 11];
+//        for (int i = 0; i <= base + 10; i++) {
+//            C[i][0] = C[i][i] = 1;
+//            for (int j = 1; j < i; j++) {
+//                C[i][j] = (C[i - 1][j - 1] + C[i - 1][j]) % 1000000007;
+//            }
+//        }
+//        int result = C[base][up];
+//        System.out.println(result);
     }
 }
--- src/算法总结/Main1.java
+++ src/算法总结/Main1.java
@@ -5,36 +5,7 @@ import java.util.Scanner;
 public class Main1 {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String[] data = scanner.nextLine().split(" ");
-        int[] array = new int[data.length];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = Integer.parseInt(data[i]);
-        }
-        int min = Integer.MAX_VALUE;
-        for (int i = 1; i < array.length / 2; i++) {
-            int count = getStepCount(i, array);
-            if (count != -1) {
-                min = Math.min(min, count);
-            }
-        }
-        if (min != Integer.MAX_VALUE) {
-            System.out.println(min);
-        } else {
-            System.out.println(-1);
-        }
-    }
 
-    private static int getStepCount(int n, int[] array) {
-        int index = n;
-        int count = 1;
-        while (index < array.length) {
-            if (index == array.length - 1) {
-                return count;
-            }
-            count++;
-            index = index + array[index];
-        }
-        return -1;
     }
+    
 }
